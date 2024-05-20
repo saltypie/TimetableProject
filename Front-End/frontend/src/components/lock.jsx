06b10ad939d3f2effbc4ded3cec2941d5e0b036b
Philip Miroga <philip.miroga@strathmore.edu>
2024-05-20 23:21:37 +0300

    frontend

--- Front-End/frontend/src/components/lock.jsx
+++ Front-End/frontend/src/components/lock.jsx
@@ -2,22 +2,54 @@ import React from 'react';
 import './Login.css';
 import { FaLock } from "react-icons/fa";
 import { Link } from 'react-router-dom';
-
+import axios from "axios";
+import{useEffect, useState} from 'react';
 
 const Lock = () => {
+
+  const [email, getEmail] = useState('');
+   const [password, setPassword] = useState('');
+   const [errorMessage, setErrorMessage] = useState('');
+
+  const submit = async e => {
+    e.preventDefault();
+    const user = {
+          email: email,
+          password: password
+         };
+    // Create the POST request
+    try {
+       const {data} = await                                                                            
+                      axios.post('http://127.0.0.1:8000/timeapp/api/login/', user ,{headers: {'Content-Type': 'application/json'}}, {withCredentials: true});
+                      // axios.post('http://127.0.0.1:8000/timeapp/api/login/', user ,{headers: {'Content-Type': 'application/json'}}, {withCredentials: true});
+ 
+  
+         localStorage.setItem('access_token', data["tokens"]["access"]);
+         localStorage.setItem('refresh_token', data["tokens"]["refresh"]);
+         localStorage.setItem('fname', data["fname"]);
+         localStorage.getItem('email');
+         localStorage.setItem('isLogged', true);
+         axios.defaults.headers.common['Authorization'] =`Bearer ${data['access']}`;
+         window.location.href = '/Home/';
+
+    } catch (error) {
+       setErrorMessage("Incorrect Password ");
+    }
+       
+ }   
   return (
     <div className='wrapper'>
-        <form action="">
+        <form action="" onSubmit={submit}>
             <h1>Locked</h1>
 
             <h3>Re-enter password to continue</h3>
 
-             <div className="input-box">
-                <input type="password" placeholder="Password" required />
-                <FaLock className='icon'/>
-             </div>
-             
-             <button type="submit" onclick="">Unlock</button>
+            <div className="input-box">
+                  <input name="password" onChange={e => setPassword(e.target.value)} value={password} type="password" placeholder="Password" required />
+                  <FaLock className='icon'/>
+               </div>
+          
+             <button type="submit" >Unlock</button>
         </form>    
     </div>
   )
