30133825d1b4e1b5c70992e2645cd2e3c6908529
Philip Miroga <philip.miroga@strathmore.edu>
2024-05-20 21:33:29 +0300

    frontend

--- Front-End/frontend/src/App.js
+++ Front-End/frontend/src/App.js
@@ -2,11 +2,14 @@ import Navigation from './components/navigation.jsx';
 import Signup from './components/signup.jsx';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Login from "./components/Login.jsx";
+import Logout from "./components/Logout.jsx";
 import Lock from "./components/lock.jsx";
 import LandingPage from "./components/landingpage.jsx";
 import Activate from "./components/Activate.jsx";
 import Home from "./components/home.jsx";
 import ResetPassword from "./components/resetPassword.jsx";
+import Email from "./components/email.jsx";
+
 import{useEffect, useState} from 'react';
 
 function App() {
@@ -54,11 +57,13 @@ useEffect(()=>{
      <BrowserRouter>
      
         <Routes>
+        <Route path="/email" element={<Email/>}/>
         <Route path="/ResetPassword" element={<ResetPassword/>}/>
           <Route path="/Home" element={<Home/>}/>
           <Route path="/Activate" element={<Activate/>}/>
           <Route path="/LandingPage" element={<LandingPage/>}/>
           <Route path="/Login" element={<Login/>}/>
+          <Route path="/Logout" element={<Logout/>}/>
           <Route path="/SignUp" element={<Signup/>}/>
           <Route path="/Lock" element={<Lock/>}/>
         </Routes>
