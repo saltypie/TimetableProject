021e049fb63cf3ce50b174bb089608ec05c56850
Philip Miroga <131269797+Philip-O-M@users.noreply.github.com>
2024-06-20 09:57:10 +0300

    Revert "Rooms"

--- Back-End/timetabling/timeapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import UserData, Role
-# Register your models here.
-
-admin.site.register(UserData)
-admin.site.register(Role)
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # TimetableProject
-
-This is a project on the creation of a timetabling app.
